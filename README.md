7a4635fcc3059905283a845f87d3c567fe838ea6
ManuDev9 <manuel.bottini@gmail.com>
2023-01-05 20:46:38 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # expenses_checker
-This is a very simple python project that let you go through your expenses, put them in buckets, and the show you how much you expended in each bucket
+This is a very simple python project that let you go through your expenses, put them in buckets, and the show you how much you expended in each bucketi
+
+You can run in this simple way:
+  python3 expenses_checker.py
+
+You need a file called "data.csv" with all your expenses.
+Each field should be divided by the delimeter ","
+One expense per line
+The first row should have the headers
+
+When run it will ask for which one is the Memo field and which one is the Amount field
+For example:
+
+Write which field is the Memo?
+dict_keys(['Number', 'Date', 'Account', 'Amount', 'Subcategory', 'Memo'])
+Memo
+Write which field is the Amount?
+dict_keys(['Number', 'Date', 'Account', 'Amount', 'Subcategory', 'Memo'])
+Amount
+
+If a trasaction without any bucket association is found (extremely common the first time you run it), the program asks for indications.
+For example
+
+This type of memo couldn't be found = 
+transact 22209536    GERMANY	ON 25 SEP ATM	
+What bucket should it belong to?
+['0 FOOD', '1 MAKESPACE', '2 DANCING', '3 DRINKS', '4 TRAVEL', '5 MUM PENSION', '6 INVESTMENTS', '7 RENT', '8 DELIVERY FOOD', '9 UNKNOWN', '10 GOING OUT', '11 VIRGIN', '12 CLOTHES', '13 SALARY', '14 PATREON', '15 MEDICINES']
+Input the index of the bucket you want to use: 10
+You choose 10 GOING OUT are you sure? (y/n) y
+What text should be associated to this memo? transact 22209536    GERMANY	ON 25 SEP ATM
+
+
+The buskets available are 
+
+['0 FOOD', '1 MAKESPACE', '2 DANCING', '3 DRINKS', '4 TRAVEL', '5 MUM PENSION', '6 INVESTMENTS', '7 RENT', '8 DELIVERY FOOD', '9 UNKNOWN', '10 GOING OUT', '11 VIRGIN', '12 CLOTHES', '13 SALARY', '14 PATREON', '15 MEDICINES']
+
+
+Which you can change from code
+
+Enjoy!
+
---
+++ expenses_checker.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python3
+
+import csv
+import json
+import os
+
+
+POSSIBLE_BUCKETS = [
+    "0 FOOD",
+    "1 MAKESPACE",
+    "2 DANCING",
+    "3 DRINKS",
+    "4 TRAVEL",
+    "5 MUM PENSION",
+    "6 INVESTMENTS",
+    "7 RENT",
+    "8 DELIVERY FOOD",
+    "9 UNKNOWN",
+    "10 GOING OUT",
+    "11 VIRGIN",
+    "12 CLOTHES",
+    "13 SALARY",
+    "14 PATREON",
+    "15 MEDICINES"]
+
+def ask_for_memo_field(fields):
+    return input("Write which field is the Memo?\n"+str(fields)+"\n")
+
+def ask_for_amount_field(fields):
+    return input("Write which field is the Amount?\n"+str(fields)+"\n")
+
+def ask_for_bucket(buckets, memo):
+    print("This type of memo couldn't be found = ")
+    print(memo)
+    print("What bucket should it belong to?")
+    print(POSSIBLE_BUCKETS)
+    bucket_index = int(input("Input the index of the bucket you want to use: "))
+    yesno = ""
+    while yesno != "y" and yesno != "n":
+        yesno = input("You choose "+ POSSIBLE_BUCKETS[bucket_index] + " are you sure? (y/n) ")
+    text_to_search = input("What text should be associated to this memo? ")
+
+    buckets[text_to_search] = POSSIBLE_BUCKETS[bucket_index]
+
+    with open("buckets.json", "w") as outfile:
+        buckets_text = json.dumps(buckets, indent=4)
+        outfile.write(buckets_text)
+
+    return [buckets, text_to_search] 
+
+def show_bucket(bucket, bucket_tot):
+    name = bucket["name"]
+    print(name+": Num Movements = "+str(bucket["num_movements"])+
+            "  ("+str(float(bucket["num_movements"])/float(bucket_tot["num_movements"])*100) +"%)")
+    print(name+": Money In = "+str(bucket["money_in"])+
+            "  ("+str(float(bucket["money_in"])/float(bucket_tot["money_in"])*100) +"%)")
+    print(name+": Money Out = "+str(bucket["money_out"])+
+            "  ("+str(float(bucket["money_out"])/float(bucket_tot["money_out"])*100) +"%)")
+    print(name+": Money Diff = "+str(bucket["money_in"] - bucket["money_out"]))
+    print("-------------------------")
+
+def main():
+    print("Reading the buckets.json file")
+    buckets = {}
+    if not os.path.isfile("buckets.json"):
+        with open("buckets.json", "w") as outfile:
+            outfile.write("{}")
+    else:
+        with open('buckets.json', 'r') as openfile:
+            buckets = json.load(openfile)
+
+    buckets_values = {}
+
+    print("Reading the data.csv file")
+    with open("data.csv","r") as data_csv:
+        csv_reader = list(csv.DictReader(data_csv))
+        memo_field = ask_for_memo_field(csv_reader[0].keys())
+        amount_field = ask_for_amount_field(csv_reader[0].keys())
+        tot_num_movements = 0
+        tot_money_in = 0
+        tot_money_out = 0
+        #header = next(csv_reader)
+        for row in csv_reader:
+            #print(row)
+            memo = row[memo_field]
+            if(row[memo_field] != None ):
+                print(row)
+                found = False
+                bucket = None
+                for text_to_search in buckets.keys():
+                    if text_to_search in memo:
+                        found = True
+                        bucket = buckets[text_to_search]
+                        break
+                if not found:
+                    [buckets, text_to_search] = ask_for_bucket(buckets, memo)
+                    bucket = buckets[text_to_search]
+                if bucket not in buckets_values:
+                    buckets_values[bucket] = {
+                        "name" : bucket,
+                        "num_movements" : 0,
+                        "money_in" : 0,
+                        "money_out" : 0
+                    }
+
+                tot_num_movements = tot_num_movements + 1
+                buckets_values[bucket]["num_movements"] = buckets_values[bucket]["num_movements"] + 1
+                amount = float(row[amount_field])
+                if amount > 0:
+                    tot_money_in = tot_money_in + amount
+                    buckets_values[bucket]["money_in"] = buckets_values[bucket]["money_in"] + amount
+                else:
+                    tot_money_out = tot_money_out - amount
+                    buckets_values[bucket]["money_out"] = buckets_values[bucket]["money_out"] - amount
+
+        bucket_values_tot = {
+            "name" : "TOT",
+            "num_movements" : tot_num_movements,
+            "money_in" : tot_money_in,
+            "money_out" : tot_money_out
+        }
+        show_bucket(bucket_values_tot, bucket_values_tot)
+        for bucket in buckets_values.keys():
+            show_bucket(buckets_values[bucket], bucket_values_tot  )
+        #print("Header is = "+str(header))
+
+
+if __name__ == "__main__":
+    main()
+
